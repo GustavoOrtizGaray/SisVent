a20b92ac03fa77d90fd7b86544c4c184791b913d
GustavoOrtizGaray <hectorgustavo.garay@gmail.com>
2019-01-17 14:42:34 -0500

    vista del login
    vista de login trabajada para revisión

--- resources/views/LoginComponent/login.blade.php
+++ resources/views/LoginComponent/login.blade.php
@@ -9,33 +9,60 @@
         	<div class="row text-center">
         		<div class="col-12">
         			<div class="container bg-light border">
-    					<div class="jumbotrom bg-secundary">
-    						logo
+        				<!--logo del sistema-->
+    					<div class="jumbotrom bg-secundary col-12 text-center">
+    						<div class="row col-offset-5">
+    							<span class="rounded-circle" style="margin: auto;">
+    								<img src="{{ asset('img_sist/logo.png') }}" class="rounded" style="width: 10em;">
+    							</span>
+    						</div>
+    						<h1>SisVent</h1> 
     					</div>
+    					<!--formulario para iniciar sesión-->
 	        			<div class="jumbotrom">
 	        				<form>
 	        					<div class="form-group">
 	        						<div class="input-group mb-3">
 									    <div class="input-group-prepend">
-									      <span class="input-group-text">@</span>
+									      <span class="input-group-text">Correo</span>
 									    </div>
-									    <input type="text" class="form-control" placeholder="Username">
+									    <input type="text" class="form-control" placeholder="Ejemplo@ejemplo.com">
 									  </div>
 			        				<div class="input-group mb-3">
 			        					 <div class="input-group-prepend">
-									      <span class="input-group-text">Password</span>
+									      <span class="input-group-text">Contraseña</span>
 									    </div>
-									    <input type="text" class="form-control" placeholder="Your Email">
+									    <input type="text" class="form-control" placeholder="contraseña">
 									    
 									  </div>
-									  <button type="button" class="btn btn-primary">login</button>
+									  <button type="button" class="btn btn-primary">Iniciar</button>
 	        					</div>
 	        				</form>
 	        			</div>
         			</div>
         		</div>
+        		<!--links de interes sobre el sistema-->
         		<div class="col-12">
-        			info
+        			<div class="row">
+        				<div class="col">
+        					<h6 class="text-center">
+		        				<a href="#"> Tutoriales </a>
+		        				<span> | </span>
+			        			<a href="#"> Videos </a>
+			        			<span> | </span>
+			        			<a href="#"> Términos y condiciones </a>
+	        				</h6>
+        				</div>
+        			</div>
+        			<div class="row">
+        				<div class="col">
+        					<h6 class="text-center">
+        						<a href="#">SisVent</a>
+        						<span> | </span>
+	        					<a href="#">Derechos Reservados</a>
+        					</h6>
+        				</div>
+        			</div>
         		</div>
         	</div>
         </div>
